af599b64476f3be2d552b2425a73bb4fdcf61d20
vancha <vancha@localhost.localdomain>
2021-03-30 14:29:19 +0200

    passing tests and cleanup

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn calculate_nr_of_parity_bits(word_length: usize) -> u32 {
 ///given a bit sequence, return the sequence with included parity bits
 fn create_parity_for_data(data: &str) -> String {
     let mut data_iter = data.chars();
-    let mut nr_of_parity_bits = calculate_nr_of_parity_bits(data.len()) as i32;
+    let nr_of_parity_bits = calculate_nr_of_parity_bits(data.len()) as i32;
     let format = (1..(data.len() + 1) as i32 + nr_of_parity_bits)
         .map(|c| {
             if (c & (c - 1)) == 0 {
@@ -25,24 +25,20 @@ fn create_parity_for_data(data: &str) -> String {
         }) //(c & (c -1))==0 means: if c is a power of two
         .collect::<String>();
 
-    let mut parity_bits: Vec<char> = vec![];
+    let mut parity_bits: Vec<char> = vec![]; //vec to store the parity bits in order
 
     for (i, p) in format.chars().enumerate() {
         //i is the index of the character in format, p is the actual char at that index
         match p {
-            //match the char
             'p' => {
-                //if it's p, that means it stands for a parity bit.
                 let mut word_iter = format[i..].chars(); //chop off the first part of the word
-                let mut sum = 0; //count the number of 1 bits
+                let mut sum = 0; //counter for the number of 1 bits
                 'inner: loop {
                     for _check in 0..i + 1 {
                         //do the checks
                         match word_iter.next() {
                             //to the the next character
                             Some(x) => {
-                                //check if it's not out of range
-                                println!("checking {},{}", x, i + 1);
                                 match x {
                                     //what's the character here
                                     '1' => {
@@ -52,7 +48,6 @@ fn create_parity_for_data(data: &str) -> String {
                                 }
                             }
                             None => {
-                                println!("done for position {},result is {}", i + 1, sum);
                                 if sum % 2 != 0 {
                                     parity_bits.push('1');
                                 } else {
@@ -63,24 +58,26 @@ fn create_parity_for_data(data: &str) -> String {
                         }
                     }
 
-                    word_iter.advance_by(i + 1);
+                    word_iter.advance_by(i + 1); //skips just as many chars as the _check loop checks, purposefully ignoring Err values.
                 }
             }
             _ => {}
         }
     }
 
-    let mut final_word_buffer: Vec<char> = vec![];
-    let mut index = 0;
+    let mut final_word_buffer: Vec<char> = vec![]; //this is going to be the buffer that's used to insert every character of the final word
+    let mut index = 0; //index to keep track of how many parity bits we have already added to the final_word_buffer
     for c in format.chars() {
+        //loop ofer format's chars (this still contains the p's that represent charity bits)
         if c == 'p' {
-            final_word_buffer.push(parity_bits[index]);
-            index += 1;
+            //if a char in format represents a parity bit
+            final_word_buffer.push(parity_bits[index]); //replace it with the correct one
+            index += 1; //increase the parity bit index
         } else {
-            final_word_buffer.push(c);
+            final_word_buffer.push(c); //if not, just push the word in there.
         }
     }
-    final_word_buffer.iter().collect::<String>()
+    final_word_buffer.iter().collect::<String>() //return the word with the replace parity bits
 }
 
 fn main() {
